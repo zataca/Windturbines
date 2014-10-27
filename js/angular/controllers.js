c05bd58ba8d9ba36f8a46ae1dd9028b17bbb3256
zataca <jeroenmartens18@gmail.com>
2014-10-27 10:38:09 +0100

    Chart fincance

--- js/angular/controllers.js
+++ js/angular/controllers.js
@@ -1,18 +1,18 @@
 /* De paginaspecifieke code zit in de controllers */
 angular.module('blogPrototype.controllers', [])
-		.controller('HomeCtrl', function($scope) {
+		.controller('HomeCtrl', function ($scope) {
 			console.info("Indexcontroller intiated")
 
 		})
 
-		.controller('OpbrengstenCtrl', function($scope) {
+		.controller('OpbrengstenCtrl', function ($scope) {
 			console.info("Indexcontroller intiated")
 		})
 
-		.directive('predictionChart', function() {
+		.directive('predictionChart', function () {
 			return {
 				restrict: 'A',
-				link: function($scope, $elem, $attr) {
+				link: function ($scope, $elem, $attr) {
 					var data = google.visualization.arrayToDataTable([
 						['Tijd', 'Voorspelde kW', 'Actuele kW'],
 						['10:00', 10000, 10200],
@@ -24,29 +24,38 @@ angular.module('blogPrototype.controllers', [])
 					]);
 					var options = {
 						title: 'Windopbrengst (kW)', aggregationTarget: 'series',
-						legend: { position: 'bottom' },
+						legend: {position: 'bottom'},
 					};
 					var chart = new google.visualization.LineChart($elem[0]);
 					chart.draw(data, options);
 				}
 			}
 		})
-		
-		.directive('profitChart', function() {
+
+		.directive('profitChart', function () {
 			return {
 				restrict: 'A',
-				link: function($scope, $elem, $attr) {
+				link: function ($scope, $elem, $attr) {
 					var data = google.visualization.arrayToDataTable([
-						['Tijd', 'Voorspelling', 'Opbrengst'],
-						['10:00', 10000, 10200],
-						['10:05', 10170, 10160],
-						['10:10', 6060, 7000],
-						['10:15', 10030, null]
+						['Year', '2014', '2013'],
+						['Jan', 1000, 400],
+						['Feb', 1170, 460],
+						['Maa', 660, 1120],
+						['Apr', 1030, 540],
+						['Mei', 100, 50],
+						['Jun', 900, 333],
+						['Jul', 555, 540],
+						['Aug', 1111, 1000],
+						['Sep', 1030, 540],
+						['Okt', 888, 999],
+						['Nov', 666, 555],
+						['Dec', 111, 1111],
 					]);
 					var options = {
-						title: 'Windopbrengst (mW)', aggregationTarget: 'series',
+						title: 'Totale opbrengst per jaar',
+						//hAxis: {title: 'Maand', titleTextStyle: {color: 'red'}}
 					};
-					var chart = new google.visualization.LineChart($elem[0]);
+					var chart = new google.visualization.ColumnChart($elem[0]);
 					chart.draw(data, options);
 				}
 			}
