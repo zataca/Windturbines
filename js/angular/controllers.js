d78d8e0e4f196aa6a9752f762a8aafcbc357ca32
zataca <jeroenmartens18@gmail.com>
2014-10-27 14:03:26 +0100

    Template

--- js/angular/controllers.js
+++ js/angular/controllers.js
@@ -60,3 +60,26 @@ angular.module('blogPrototype.controllers', [])
 				}
 			}
 		})
+
+		.directive('dayChart', function () {
+			return {
+				restrict: 'A',
+				link: function ($scope, $elem, $attr) {
+					var data = google.visualization.arrayToDataTable([
+						['Tijd', 'Voorspelde kW', 'Actuele kW'],
+						['10:00', 10000, 10200],
+						['10:05', 10170, 10160],
+						['10:10', 6060, 7000],
+						['10:15', 10030, null],
+						['10:30', 1, null],
+						['10:45', 1030, null]
+					]);
+					var options = {
+						title: 'Windopbrengst (kW)', aggregationTarget: 'series',
+						legend: {position: 'bottom'},
+					};
+					var chart = new google.visualization.LineChart($elem[0]);
+					chart.draw(data, options);
+				}
+			}
+		})
